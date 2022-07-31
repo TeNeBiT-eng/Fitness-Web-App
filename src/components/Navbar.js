8a5b1cd648e076e82f327403ac34bec3a6974b7b
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-07-31 15:48:34 +0100

    hero-banner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
-    <Stack direction="row" justifyContent="space-around" sx={{ gap: {sm: '122px', xs: '40px'}}}>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{ gap: { sm: "122px", xs: "40px" }, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none' }} px='20px'
+    >
       <Link to="/">
         <img
           src={Logo}
