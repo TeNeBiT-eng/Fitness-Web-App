f3fc7aab17253a4496bd76b2badcb54d087c559f
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-01 15:07:37 +0100

    exercise result from search exercise

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,17 +3,32 @@ import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from "../utilities/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
+  console.log(exercises);
   return (
   <Box id='exercises'
     sx={{ mt: {lg: '110px'}}}
     mt='50px'
     p='20px'
   >
-    <Typography variant="h4">
+    <Typography variant="h4" mb='46px'>
       Showing Results
     </Typography>
+
+    <Stack 
+      direction='row'
+      sx={{
+        gap: { lg: '110px', xs: '50px'}
+      }}
+      flexWrap='wrap'
+      justifyContent='center'
+    >
+      {exercises.map((exercise, index) => (
+        <ExerciseCard key={index} exercise={exercise} />
+      ))}
+    </Stack>
   </Box>
   )
 };
