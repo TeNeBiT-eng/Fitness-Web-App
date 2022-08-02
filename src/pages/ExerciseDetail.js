8ca709dd76fd4999601f5f779a0b8c9d9c0591a9
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-02 13:28:12 +0100

    exercise details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData, youtubeOptions } from "../utilities/fetchData";
+
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([])
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      const ExerciseVideoData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(ExerciseVideoData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
-    <div>ExerciseDetail</div>
-  )
-}
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseVideos.name}/>
+      <SimilarExercises />
+    </Box>
+  );
+};
 
 export default ExerciseDetail;
--- src/utilities/fetchData.js
+++ src/utilities/fetchData.js
@@ -1,9 +1,17 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY
   },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+  }
 };
 
 export const fetchData = async (url, options) => {
