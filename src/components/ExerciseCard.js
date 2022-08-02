cb91eb20223b28bc0ff6c56453b9a871542c1454
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-02 08:30:14 +0100

    exercise pagination and home page finalized

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -6,6 +6,44 @@ const ExerciseCard = ({ exercise }) => {
   return (
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            backgroundColor: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            backgroundColor: "#fcc757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        ml="23px"
+        color="#000"
+        fontWeight="bold"
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize='20px'
+      >
+        {exercise.name}
+      </Typography>
     </Link>
   );
 };
