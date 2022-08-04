900c04dd8d82bbbdef4903b383e3eaa23af6d72b
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-04 12:28:44 +0100

    footer completion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <Box mt="80px" color="#fff3f4">
+    <Box mt="80px" backgroundColor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="logo" width="200px" height="40px" />
-        <Typography variant="h5" pb='40px' mt='20px'>
+        <Typography variant="h5" pb='40px' mt='20px' color='#000'>
           Made with ❤️ of fitness by Oluwatomisin Babalola
         </Typography>
       </Stack>
